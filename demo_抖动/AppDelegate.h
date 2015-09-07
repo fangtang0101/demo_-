5b8572fe00a887b3d2d48a86d9d6755c5d727f34
fangtang0101 <fang623003648>
2015-09-07 09:25:00 +0800

    Initial Commit

---
+++ demo_抖动/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo_抖动
+//
+//  Created by 方春高 on 15/9/7.
+//  Copyright (c) 2015年 方春高. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo_抖动/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo_抖动
+//
+//  Created by 方春高 on 15/9/7.
+//  Copyright (c) 2015年 方春高. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
